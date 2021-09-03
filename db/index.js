7c5b2470519ba71aa32c5e15a08805e52b00c86e
mhydara0624 <mhydara0624@icloud.com>
2021-09-03 11:51:27 -0400

    trying deployment

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/productsDatabase'
+// let MONGODB_URI = 'mongodb://127.0.0.1:27017/productsDatabase'
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
